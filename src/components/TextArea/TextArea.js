cff3b9a0a37753d872634f21d332398d28882694
Dan Kennedy <dannykennedy@email.com>
2019-04-06 10:39:08 -0400

    styling

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/textarea.css'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
